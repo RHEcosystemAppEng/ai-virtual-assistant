e005f0e7d811c930e4487c77c6ab47c8757609cf
Sid Kattoju <skattoju@redhat.com>
2025-05-15 14:02:26 -0400

    adding basic session management

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 import os
 from .models import Base
 
@@ -18,3 +18,10 @@ AsyncSessionLocal = sessionmaker(
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=Session,
+    autoflush=False,
+    autocommit=False
+)
